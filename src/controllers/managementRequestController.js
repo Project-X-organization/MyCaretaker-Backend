a17404aa99a359692094e0c3df9acb6722708644
samuelfreeman <samuelmanueljnr@gmail.com>
2025-02-09 02:29:04 +0000

    included the management request controller

---
+++ src/controllers/managementRequestController.js
@@ -0,0 +1,41 @@
+const { prisma } = require("../utils/prismaUtill");
+
+exports.submitManagementRequest = async (req, res) => {
+  try {
+    const { propertyId, userId } = req.body;
+
+    const property = await prisma.property.findUnique({
+      where: {
+        id: propertyId,
+      },
+    });
+    if (!property) {
+      throw new Error("Property not found");
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const managementRequest = await prisma.managementRequest.create({
+      data: {
+        propertyId,
+        userId,
+      },
+    });
+    if (!managementRequest) {
+      throw new Error("Error creating management request");
+    }
+    res.status(201).json({
+      managementRequest,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error creating management request",
+      error: error.message,
+    });
+  }
+};
